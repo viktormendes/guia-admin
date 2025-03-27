68329d72e9a7945fd4f58a3b60c2e8c80d606d35
dvktr <victormendes04012004@gmail.com>
2025-03-27 19:49:58 -0300

    (GUIA): adjust timetable type to recive rooms

--- types/Timetable.ts
+++ types/Timetable.ts
@@ -1,6 +1,14 @@
+import { Room } from "@/app/dashboard/classrooms/types"
+
 export interface ITimetable {
   id: number;
   disciplineId: number;
   days: string;
   hours: string;
+  educator: {
+    id: number;
+    name: string;
+    lattesLink: string;
+  };
+  room?: Room;
 }
